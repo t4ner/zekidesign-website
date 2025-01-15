fa782c3f822c6df1eb94ba97472d534649ec45ad
Taner <tanerdokmets@gmail.com>
2025-01-15 12:59:33 +0300

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,15 +5,15 @@ import products from "../data";
 const Products = () => {
   return (
     <div className="relative flex flex-col items-center w-full min-h-screen px-4 md:px-8">
-      <div className="flex flex-col items-center justify-center mt-32 space-y-3 w-[70%]">
-        <h1 className="text-3xl font-bold text-[#E40128]">
+      <div className="flex flex-col items-center justify-center mt-16 space-y-3 md:mt-32 ">
+        <h1 className="md:text-3xl text-xl text-center font-bold text-[#E40128]">
           PRINT- UND WERBETECHNIK: KREATIV SEIT 2002
         </h1>
-        <p className="w-[65%]  font-medium text-center text-[#06234B]">
+        <p className="md:w-[65%]  font-medium text-center text-[#06234B]">
           „Ihr Partner für innovative Druck- und Werbelösungen.“
         </p>
       </div>
-      <div className="w-full mt-32">
+      <div className="w-full mt-16 md:mt-32">
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
           {products.map((p) => (
             <Link
@@ -23,10 +23,10 @@ const Products = () => {
             >
               <img
                 src={p.img}
-                className="object-cover w-36 h-36 md:h-44 md:w-44"
+                className="object-cover w-28 h-28 md:h-44 md:w-44"
                 alt={p.title}
               />
-              <p className="mt-5 text-lg font-semibold text-center">
+              <p className="mt-5 text-base font-semibold text-center md:text-lg">
                 {p.title}
               </p>
               <div className="mt-1 space-y-1 font-medium text-center text-gray-500">
